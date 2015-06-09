26ca116aaa972129181a46c93612a61950f6eb63
Jeremy Douglas Hoover <jeremy@jeremyhoover.ca>
2015-06-09 14:00:45 -0400

    Update analytics_events.js

--- dev/js/blocks/analytics_events.js
+++ dev/js/blocks/analytics_events.js
@@ -1,13 +1,16 @@
-  $(function() {
+$(function() {
 
     $('a[href^="tel"]').on('click', function() {
-      this_rel = $(this).attr('rel');
-      ga('send', 'event', 'phone', 'click', this_rel);
+        var this_rel = $(this).attr('rel');
+        if (typeof attr !== typeof undefined && attr !== false) {
+            this_rel = 'general';
+        }
+        ga('send', 'event', 'phone', 'click', this_rel);
     });
 
     $(window).scroll(function() {
-       if($(window).scrollTop() + $(window).height() == $(document).height()) {
-           ga('send', 'event', 'window', 'scroll', 'bottom');
-       }
+        if($(window).scrollTop() + $(window).height() == $(document).height()) {
+            ga('send', 'event', 'window', 'scroll', 'bottom');
+        }
     });
-  });
+});
