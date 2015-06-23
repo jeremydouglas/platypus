c3afe5e31c4b9827af1a03d1f75ab802313fd9eb
Jeremy Hoover <jeremy@compasscreative.ca>
2015-06-23 09:07:47 -0400

    proper css compression and update .htaccess

--- gulpfile.js
+++ gulpfile.js
@@ -54,7 +54,7 @@ gulp.task('css', function () {
     extensions: ['.scss']
   }))
   .pipe(sass({
-    style: 'compressed',
+    outputStyle: 'compressed',
     errLogToConsole: false
   }))
   .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
